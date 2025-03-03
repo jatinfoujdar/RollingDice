641123252cdd7b08aa9189061fbbda38324d2be8
jatin foujdar <jatin321001@gmail.com>
2025-03-03 20:05:35 +0530

    random number commit

--- app/src/main/java/com/example/rollingdice/MainActivity.kt
+++ app/src/main/java/com/example/rollingdice/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.rollingdice
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked" , Toast.LENGTH_SHORT).show()
+           rollDice()
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -23,5 +25,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        val randomInt = Random.nextInt(6) + 1
+
+        resultText.text = randomInt.toString()
     }
 }
